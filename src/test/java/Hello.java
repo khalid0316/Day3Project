ee9c61253578592428aed2dbeb6080e26b5161d5
khalid0316 <khalid.mowafi@gmail.com>
2021-05-22 11:21:36 -0400

    added new lines

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -3,5 +3,8 @@ public class Hello {
     public static void main(String[] args) {
 
         System.out.println("Hello World");
+        // add one more line
+        //enhancing hello
+        //observe commit
     }
 }
